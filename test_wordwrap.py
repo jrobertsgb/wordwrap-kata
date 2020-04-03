2940762afe289672545d6149383703f7abf29ad2
Joe Roberts <joe.roberts@madetech.com>
2020-04-03 14:56:20 +0100

    Wrap text test

--- test_wordwrap.py
+++ test_wordwrap.py
@@ -2,10 +2,22 @@ import unittest
 from wordwrap import Wrapper
 
 class TestWordWrapper(unittest.TestCase):
+    def setUp(self):
+        self.wrapper = Wrapper()
+
     def test_can_initialise_a_wrapper(self):
         wrapper = Wrapper()
+
+        assert wrapper is not None
 
     def test_can_wrapper_accept_text(self):
         wrapper = Wrapper()
 
         wrapper.wrap("somedata")
+
+    def test_wrapper_returns_text(self):
+        text = "foobar"
+
+        wrapped = self.wrapper.wrap(text)
+
+        assert wrapped == text
--- wordwrap.py
+++ wordwrap.py
@@ -1,3 +1,3 @@
 class Wrapper:
     def wrap(self, unwrapped_text):
-        pass
+        return "foobar"
